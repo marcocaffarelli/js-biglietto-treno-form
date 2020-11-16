a05f368f3942c4dfd5f1c6caafbd4536b221bb15
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-16 16:24:26 +0100

    Aggiunti i dati alla sezione il tuo biglietto
    Aggiunti i dati alla sezione il tuo biglietto

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,21 @@ var btnAnnulla = document.getElementById("btn_annulla");
 btnGenera.addEventListener('click', function() {
    // console.log("genera");
 
+   // var degli id dei tag input
    var nomeUtente = document.getElementById("nome_cognome")
    var kmUtente = document.getElementById("km")
    var fasciaEtaUtente = document.getElementById("fascia_età")
 
-
+   // var degli id dei tag input a cui è assegnata il value
    var nomeCognome = nomeUtente.value;
    var km = kmUtente.value;
    var fasciaEta = fasciaEtaUtente.value;
    // console.log(nomeCognome, km, fasciaEta);
 
+   var costoAlKm = 0.21;
+   var costoBiglietto = costoAlKm * km;
 
+  
 });
 
 btnAnnulla.addEventListener('click', function() {
