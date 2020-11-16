24093100842dc9e71bd4b2831a57dfdfff36dc5c
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-16 15:45:39 +0100

    Aggiunti id agli input da compilare
    Aggiunti id agli input da compilare e aggiunti  in javascript i value in modo da poterli utilizzare poi per la creazione del biglietto

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,17 @@ var btnAnnulla = document.getElementById("btn_annulla");
 btnGenera.addEventListener('click', function() {
    // console.log("genera");
 
+   var nomeUtente = document.getElementById("nome_cognome")
+   var kmUtente = document.getElementById("km")
+   var fasciaEtaUtente = document.getElementById("fascia_età")
+
+
+   var nomeCognome = nomeUtente.value;
+   var km = kmUtente.value;
+   var fasciaEta = fasciaEtaUtente.value;
+   // console.log(nomeCognome, km, fasciaEta);
+
+
 });
 
 btnAnnulla.addEventListener('click', function() {
