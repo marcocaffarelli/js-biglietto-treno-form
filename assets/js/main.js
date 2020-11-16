b16fe43b77b2b06ae3f53677755eaa6a135a2430
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-16 15:32:15 +0100

    Verificato il funzionamento al click
    Verificato il funzionamento al click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,13 @@
+// variabili bottoni
 var btnGenera = document.getElementById("btn_genera");
 var btnAnnulla = document.getElementById("btn_annulla");
+
+
+btnGenera.addEventListener('click', function() {
+   // console.log("genera");
+
+});
+
+btnAnnulla.addEventListener('click', function() {
+  // console.log("annulla");
+});
